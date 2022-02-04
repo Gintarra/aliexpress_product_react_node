72eb168ffe8a82ab9128d55cdf77515a674ff7b5
Gintare Raudziute <gintare.raudziute@gmail.com>
2022-02-04 18:03:37 +0200

    add go back button

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,32 +6,40 @@ import { MyContext } from '../context/MyContext';
 const Main = () => {
     const input = useRef();
     const navigate = useNavigate();
-    const {setProduct, getProduct} = useContext(MyContext)
+    const { setProduct, getProduct } = useContext(MyContext)
     const [getProductLoad, setProductLoad] = useState(false)
+    const [getError, setError] = useState(false)
 
     async function getInfo() {
-        setProductLoad(true)
-        const id = input.current.value;
-        console.log(id);
-        const options = {
-            method: "POST",
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({id})
+        if (input.current.value.length > 0) {
+            setError(false)
+            setProductLoad(true)
+            const id = input.current.value;
+            console.log(id);
+            const options = {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ id })
+            }
+            const result = await fetch('http://localhost:4000/product', options)
+            const data = await result.json()
+            console.log(data.data, "grizo")
+            setProduct(data.data)
+            setProductLoad(false)
+            navigate('/product')
+        } else {
+            setError(true)
         }
-        const result = await fetch('http://localhost:4000/product', options)
-        const data = await result.json()
-        console.log(data.data, "grizo")
-        setProduct(data.data)
-        setProductLoad(false)
-        navigate('/product')
+
     }
     return (
         <div className='d-flex column align-center mt-50'>
             <input type="text" placeholder='write product ID' ref={input} />
             <button onClick={getInfo}>GET INFO</button>
-           {getProductLoad && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
+            {getError && <h4>Error: Empty input field!</h4>}
+            {getProductLoad && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
         </div>
     );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MyContext } from '../context/MyContext';
 
 const Product = () => {
     const { getProduct } = useContext(MyContext)
     const [getPhoto, setPhoto] = useState(getProduct.images[0])
-    function changePhoto(x){
+    const navigate = useNavigate()
+    const starUrl ="https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=";
+    const noStarUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"
+    function changePhoto(x) {
         setPhoto(x)
     }
-    function changePhoto2(){
-       const photoIndex =  getProduct.images.indexOf(getPhoto)
-       if(photoIndex !== -1 && photoIndex+1 < getProduct.images.length){
-            setPhoto(getProduct.images[photoIndex+1])
-       } else {
-        setPhoto(getProduct.images[0])
-       }
+    function changePhoto2() {
+        const photoIndex = getProduct.images.indexOf(getPhoto)
+        if (photoIndex !== -1 && photoIndex + 1 < getProduct.images.length) {
+            setPhoto(getProduct.images[photoIndex + 1])
+        } else {
+            setPhoto(getProduct.images[0])
+        }
+    }
+    function goBack() {
+        navigate('/')
     }
     return (
         <div className='m-20'>
+            <div className='d-flex justify-right'>
+                <button onClick={goBack}>Go back</button>
+            </div>
             <div className='d-flex justify-center'>
 
                 {getProduct &&
@@ -44,19 +54,19 @@ const Product = () => {
                             <div className='grow2'>
                                 <p>{x.date}</p>
                                 <img className="stars"
-                                    src={x.rating >= 1 ? "https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"}
+                                    src={x.rating >= 1 ? starUrl: noStarUrl}
                                     alt="" />
                                 <img className="stars"
-                                    src={x.rating >= 2 ? "https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"}
+                                    src={x.rating >= 2 ? starUrl: noStarUrl}
                                     alt="" />
                                 <img className="stars"
-                                    src={x.rating >= 3 ? "https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"}
+                                    src={x.rating >= 3 ? starUrl: noStarUrl}
                                     alt="" />
                                 <img className="stars"
-                                    src={x.rating >= 4 ? "https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"}
+                                    src={x.rating >= 4 ?starUrl: noStarUrl}
                                     alt="" />
                                 <img className="stars"
-                                    src={x.rating >= 5 ? "https://media.istockphoto.com/vectors/star-icon-vector-id1129712692?k=20&m=1129712692&s=612x612&w=0&h=LHyR3dyiTXewDgGh6HGL4Hvky1s4veeEkr3VE05N5ww=" : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empty_Star.svg/1200px-Empty_Star.svg.png"}
+                                    src={x.rating >= 5 ? starUrl: noStarUrl}
                                     alt="" />
                                 <p>{x.content}</p>
                             </div>
